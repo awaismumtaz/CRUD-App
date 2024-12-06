346d295e62ff22ae476cfadcc81a59a9e6a47ed0
Awais Mumtaz <awaismumtaz199@gmail.com>
2024-12-06 01:50:39 +0100

    Added concert class

---
+++ CRUD-App/Concert.cs
@@ -0,0 +1,21 @@
+namespace CRUD_App;
+
+public class Concert
+{
+    public int Id { get; set; }
+    public string Venue { get; set; }
+    public string Performer { get; set; }
+    public int Capacity { get; set; }
+    public DateTime SpecificTime { get; set; }
+
+    public Concert(int id, string venue, string performer, int capacity, DateTime specifictime)
+    {
+        Id = id;
+        Venue = venue;
+        Performer = performer;
+        Capacity = capacity;
+        SpecificTime = specifictime;
+    }
+    
+   
+}
